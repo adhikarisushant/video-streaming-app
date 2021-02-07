1794652c546ed06dc8c6bbaa415411c96e94df51
Sushant <sushant.adhikari.619@gmail.com>
2021-02-07 09:20:03 +0545

    fetching data to banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BannerImg from '../../assets/banner.jpg'
 import './Banner.css'
+import axios from '../../axios'
+import requests from '../../Requests'
 
 const Banner = () => { 
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    }, []);
+
+    console.log(movie);
 
     function truncate(string, n) {
             return string?.length > n ? string.substr(n, n - 1) + '...' : string
@@ -10,19 +28,19 @@ const Banner = () => {
     return (
         <header className="banner" style={{
             backgroundSize: "cover",
-            backgroundImage: `url(${BannerImg})`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center",
         }}>
             <div className="banner_contents">
                 <h1 className="banner_title">
-                    Gone Girl
+                    {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner_buttons">
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
                 </div>
                 <h1 className="banner_description">
-                    {truncate(`Magic of David Fincher.`, 150)}
+                    {truncate(movie?.overview, 150)}
                     </h1>
             </div>
             <div>
